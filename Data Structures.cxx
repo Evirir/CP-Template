19de1282813dc7b48576e0b9f44c451fce4bdad0
Evirir <gojunxing@gmail.com>
2019-09-16 21:16:19 +0800

    Fixed Lazy Segment Tree range push()

--- Data Structures.cxx
+++ Data Structures.cxx
@@ -63,12 +63,13 @@ public:
 		lazy.resize(size_*4);
 	}
 	void clear(){
+		v.clear(); lazy.clear();
 		v.resize(size_*4,0);
 		lazy.resize(size_*4,0);
 	}
 	inline void push(int k, int l, int r){
 		if(lazy[k]!=0){
-			v[k]+=lazy[k];
+			v[k]+=(r-l+1)*lazy[k];	//remember to consider the range!
 			if(l!=r){
 				lazy[k*2]+=lazy[k];
 				lazy[k*2+1]+=lazy[k];
@@ -87,6 +88,54 @@ public:
 	}
 };
 //Lazy recursive ST end
+
+//Point update ST start
+class LazySegmentTree{
+private:
+	int size_;
+	vector<ll> v;
+	
+	void update(int s, int e, ll val, int k, int l, int r){
+		if(r < s || e < l) return;
+		if(s <= l && r <= e){
+			v[k] = v[k*2] | v[k*2+1];
+		}
+		else{
+			update(s, e, val, k*2, l, (l+r)>>1);
+			update(s, e, val, k*2+1, ((l+r)>>1)+1, r);
+			v[k] = merge(v[k*2], v[k*2+1]);
+		}
+	}
+	
+	ll query(int s, int e, int k, int l, int r){
+		if(r < s || e < l) return 0;
+		if(s <= l && r <= e) return v[k];
+		ll lc = query(s, e, k*2, l, (l+r)>>1);
+		ll rc = query(s, e, k*2+1, ((l+r)>>1)+1, r);
+		return merge(lc, rc);
+	}
+ 
+public:
+	LazySegmentTree(): v(vector<ll>())) {};
+	LazySegmentTree(int n){
+		for(size_=1;size_<n;) size_<<=1;
+		v.resize(size_*4);
+	}
+	void clear(){
+		v.clear();
+		v.resize(size_*4,0);
+	}
+	inline ll merge(ll x, ll y){
+		return x|y;
+	}
+	inline void update(int l, int r, ll val){
+		update(l, r, val, 1, 0, size_-1);
+	}
+	inline ll query(int l, int r){
+		return query(l, r, 1, 0, size_-1);
+	}
+};
+//Point update ST end
 
 //DSU start
 struct DSU{
