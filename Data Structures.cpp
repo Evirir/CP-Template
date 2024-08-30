76eeb88e1d791330f1b47e245b69ca21fc0cd617
Evirir <gojunxing@gmail.com>
2024-08-30 12:46:13 +0800

    Update base template

--- Data Structures.cpp
+++ Data Structures.cpp
@@ -6,9 +6,6 @@ using namespace std;
 
 #pragma GCC optimize("O3,unroll-loops")
 #pragma GCC target("avx2")
-#define watch(x) cout<<(#x)<<"="<<(x)<<'\n'
-#define mset(d,val) memset(d,val,sizeof(d))
-#define cbug if(DEBUG) cout
 #define setp(x) cout<<fixed<<setprecision(x)
 #define sz(x) (int)(x).size()
 #define all(x) begin(x), end(x)
@@ -27,10 +24,9 @@ typedef vector<ii> vii;
 //template<typename T>
 //using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 void SD(int t=0){ cout<<"PASSED "<<t<<endl; }
-ostream& operator<<(ostream &out, ii x){ out<<"("<<x.F<<","<<x.S<<")"; return out; }
+
 const ll INF = ll(1e18);
 const int MOD = 998244353;
-
 const bool DEBUG = 0;
 const int MAXN = 100005;
 const int LG = 21;
